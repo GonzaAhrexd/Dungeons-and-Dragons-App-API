5d816e77d213d13cab566f8a5514606f10f2fab2
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2026-07-01 19:56:58 -0300

    feat(auth): Add password encryptation and username as unique key

--- src/modules/auth/features/register/register.service.ts
+++ src/modules/auth/features/register/register.service.ts
@@ -1,14 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from '../../schema/user.schema';
 import { RegisterDto } from './register.dto';
-
 @Injectable()
 export class RegisterService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async execute(dto: RegisterDto) {
-    return this.userModel.create(dto);
+    try {
+      const user = await this.userModel.create(dto);
+      const { ...result } = user.toObject();
+      return result;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Username already exists');
+      }
+      throw error;
+    }
   }
 }
--- src/modules/auth/schema/user.schema.ts
+++ src/modules/auth/schema/user.schema.ts
@@ -1,17 +1,23 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop({ required: true, select: false })
+  @Prop({ required: true, unique: true, trim: true })
+  username!: string;
+  @Prop({ required: true, trim: true })
   password!: string;
 
-  @Prop({ required: true, trim: true })
-  username!: string;
   @Prop({ trim: true })
   campaigns!: string[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
+
+UserSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
